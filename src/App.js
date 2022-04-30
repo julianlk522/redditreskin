2010721d970e47b0c2f278c523c354ee3cb14f95
Julian Lindsay-Kaufman <jjjuliannnn456@gmail.com>
2022-04-30 13:33:32 -0400

    added react icons, changed app.js so that fetchSubreddit is only run when the search icon is clicked not when state changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import {FaSearch} from 'react-icons/fa'
 // import {RedditProvider} from "./RedditContext";
 import Post from "./components/Post";
 import Header from "./components/Header";
@@ -12,33 +13,34 @@ function App() {
   const redditURL = `https://www.reddit.com/r/${subreddit}.json`
   
   useEffect(() => {
-    const fetchSubreddit = async () => {
-      try {
-          setLoading(true)
-        
-          const rawData = await fetch(redditURL)
-          const jsonData = await rawData.json()
-          
-          if (jsonData !== null) {
-            setPosts(jsonData.data.children)
-            setLoading(false)
-          }
-      } catch (error) {
-          console.log(error)
-          setLoading(false)
-      }
-    }
-  
     fetchSubreddit()    
-  }, [subreddit])
+  }, [])
 
-  // if (loading) return <Spinner />
+  const fetchSubreddit = async () => {
+    try {
+        setLoading(true)
+      
+        const rawData = await fetch(redditURL)
+        const jsonData = await rawData.json()
+        
+        if (jsonData !== null) {
+          setPosts(jsonData.data.children)
+          setLoading(false)
+        }
+    } catch (error) {
+        console.log(error)
+        setLoading(false)
+    }
+  }
+
+  if (loading) return <Spinner />
   
   return (
     // <RedditProvider>
         <>
         <header>
           <input type="text" className="input" value={subreddit} onChange={(e) => setSubreddit(e.target.value)}/>
+          <FaSearch onClick={fetchSubreddit}/>
         </header>
         {(posts !== null) 
           ? posts.map((post, index) => <Post key={index} postInfo={post.data}/>)
